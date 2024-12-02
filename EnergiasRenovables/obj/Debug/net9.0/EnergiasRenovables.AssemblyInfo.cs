16e72921efd44af949109458b7560a2b3334f071
luifer991 <luifer991@gmail.com>
2024-12-02 07:04:24 -0500

    all changes for files

--- EnergiasRenovables/obj/Debug/net9.0/EnergiasRenovables.AssemblyInfo.cs
+++ EnergiasRenovables/obj/Debug/net9.0/EnergiasRenovables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergiasRenovables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1688e6dcf7c6301849fd13a2cbf98294095e517")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d1cd6e35a59d92166e421ae0ffaf9d2cafa823")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergiasRenovables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnergiasRenovables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
