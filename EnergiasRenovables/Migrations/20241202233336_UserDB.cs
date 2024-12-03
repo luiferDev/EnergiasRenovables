0469c52c23d04af63a179adafb62309660c70ccc
luifer991 <luifer991@gmail.com>
2024-12-02 20:08:08 -0500

    minimal changes

---
+++ EnergiasRenovables/Migrations/20241202233336_UserDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnergiasRenovables.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Enail = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
---
+++ EnergiasRenovables/Migrations/20241203003727_UserDBCorrected.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnergiasRenovables.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserDBCorrected : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Enail",
+                table: "Usuarios");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Usuarios",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Usuarios");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Enail",
+                table: "Usuarios",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
