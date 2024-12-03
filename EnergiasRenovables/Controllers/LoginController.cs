d82a6d5f70c3e9ec782359273fecc1d9350b983f
luifer991 <luifer991@gmail.com>
2024-12-02 23:24:06 -0500

    login controller

---
+++ EnergiasRenovables/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+using EnergiasRenovables.Model.DTO;
+using EnergiasRenovables.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnergiasRenovables.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoginController(AuthService authService): ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> LoginUsuario([FromBody] UsuarioLoginDTO login)
+    {
+        var token = await authService.LoginAsync(login.UserName, login.Password);
+        if (token == null)
+            return Unauthorized(new { Message = "Credenciales inválidas." });
+
+        return Ok(new { Token = token });
+    }
+}
