7343f7ac23cd3832190706c48843038c44e4aa57
luifer991 <luifer991@gmail.com>
2024-12-26 16:04:30 -0500

    correct

--- EnergiasRenovables/obj/Debug/net9.0/EnergiasRenovables.AssemblyInfo.cs
+++ EnergiasRenovables/obj/Debug/net9.0/EnergiasRenovables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergiasRenovables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a3be13c32a5af5df453742cb91ebbf85758ac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7bb8bad66c53d6be2e5a01593ab0cece3bc7ce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergiasRenovables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnergiasRenovables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
